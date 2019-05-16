d8570ca075f3b774626d086de0644d33b600c5bc
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-16 14:16:39 -0400

    Jest and supertest

---
+++ api/server.spec.js
@@ -0,0 +1,35 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('server', () => {
+	it('sets the environment to testing', () => {
+		expect(process.env.DB_ENV).toBe('testing');
+	});
+
+	describe('GET /', () => {
+		it('should return 200 OK', () => {
+			return request(server).get('/').expect(200);
+		});
+
+		it('using the squad (async/await)', async () => {
+			// use the squad
+			const res = await request(server).get('/');
+			expect(res.status).toBe(200);
+		});
+
+		it('should return JSON using done callback', done => {
+			// using the done callback
+			request(server).get('/').then(res => {
+				expect(res.type).toBe('application/json'); // Content-Type
+				done();
+			});
+		});
+		it('should return {api: "up"}', () => {
+			const expected = { api: 'up' };
+
+			return request(server).get('/').then(res => {
+				expect(res.body).toEqual(expected);
+			});
+		});
+	});
+});
